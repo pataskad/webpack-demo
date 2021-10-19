5fa541259e28bdfabd77188fd36c0838497b5d19
Dallas Pataska <pataskad@gmail.com>
2021-10-19 13:41:44 -0500

    added asset and output managemnet along with webpack dev server

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import _ from 'lodash'
 import myName from './myName'
+import './style.css'
+import Logo from './iracing-blue-logo.png'
+
+import printMe from './print.js'
 
 function component() {
     const element = document.createElement('div')
+    const btn = document.createElement('button')
 
     // Lodash, now imported by this script
     element.innerHTML = myName('Dallas')
+    element.classList.add('hello')
+
+    btn.innerHTML = 'Click me and check the console!'
+    btn.onclick = printMe
+
+    element.appendChild(btn)
+
+    const myIcon = new Image()
+    myIcon.src = Logo
+
+    element.appendChild(myIcon)
 
     return element
 }
