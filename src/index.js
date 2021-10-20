ab5ed5bd3ef9ae80482d3767831c6a1bc6b35044
Dallas Pataska <pataskad@gmail.com>
2021-10-20 14:50:12 -0500

    back to basic webpack structure

--- src/index.js
+++ src/index.js
@@ -1,27 +1,10 @@
 import _ from 'lodash'
-import myName from './myName'
-import './style.css'
-import Logo from './iracing-blue-logo.png'
-
-import printMe from './print.js'
 
 function component() {
-    const element = document.createElement('div')
-    const btn = document.createElement('button')
-
+    const element = document.createElement('div');
+    
     // Lodash, now imported by this script
-    element.innerHTML = myName('Dallas')
-    element.classList.add('hello')
-
-    btn.innerHTML = 'Click me and check the console!'
-    btn.onclick = printMe
-
-    element.appendChild(btn)
-
-    const myIcon = new Image()
-    myIcon.src = Logo
-
-    element.appendChild(myIcon)
+    element.innerHTML = _.join(['Hello', 'webpack!'], ' ')
 
     return element
 }
